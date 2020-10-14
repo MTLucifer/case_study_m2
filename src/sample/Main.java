d759c3a24a6ab29b396dd05882142c4c181ee3cc
MTLucifer <nguyenthanhtien.vnuf@gmail.com>
2020-10-14 12:33:50 +0700

    new

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,16 +14,6 @@ public class Main extends Application {
         primaryStage.setTitle("Cat Hotel Management");
         primaryStage.setScene(new Scene(root, 1100, 700));
         primaryStage.show();
-//        FileChooser fileChooser = new FileChooser();
-//        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PDF File", "*.pdf"));
-//        fileChooser.setTitle("Save To PDF");
-//        fileChooser.setInitialFileName("untitle.pdf");
-//        File file = fileChooser.showSaveDialog(primaryStage);
-//        if (file != null) {
-//            String str = file.getAbsolutePath();
-//            FileOutputStream fos = new FileOutputStream(str);
-//            PD pdf =
-//        }
     }
 
     public static void main(String[] args) {
